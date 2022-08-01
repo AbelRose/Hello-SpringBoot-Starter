3be0e8462e2b7b937426ea83fa14b456db970fda
yihaosun <sunyh@smartebao.com>
2022-08-01 15:49:32 +0800

    [INIT] init unified-messaging-sdk project

---
+++ GitLab/Unified-Messaging-SDK/src/main/java/com/unified/messaging/sdk/unifiedmessagingsdk/UnifiedMessagingSdkApplication.java
@@ -0,0 +1,13 @@
+package com.unified.messaging.sdk.unifiedmessagingsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiedMessagingSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnifiedMessagingSdkApplication.class, args);
+    }
+
+}
---
+++ GitLab/Unified-Messaging-SDK/src/test/java/com/unified/messaging/sdk/unifiedmessagingsdk/UnifiedMessagingSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unified.messaging.sdk.unifiedmessagingsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedMessagingSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
